d4842f7150bdc1a737f624015d87cbb3ff37366b
CCurl <curlbud-github@yahoo.com>
2015-09-28 13:51:01 -0400

    added command line params

--- FCon/FCon.cpp
+++ FCon/FCon.cpp
@@ -23,7 +23,11 @@ FILE *fp = NULL;
 char line[128];
 ForthOS *myOS = NULL;
 
-void go()
+int init();
+int startup();
+void SignalHandler(int action);
+
+int go()
 {
 	while (true)
 	{
@@ -41,22 +45,13 @@ void go()
 		CString input(line);
 		if (input.CompareNoCase(_T("bye")) == 0)
 		{
-			return;
+			return 0;
 		}
 
 		if (input.CompareNoCase(_T("restart")) == 0)
 		{
-			delete myOS;
-			myOS = NULL;
 			strcpy_s(line, sizeof(line), ".bs");
 			len = 3;
-		}
-
-		if (myOS == NULL)
-		{
-			myOS = new ForthOS(MEMORY_SIZE);
-			myOS->output_fp = stdout;
-			myOS->BootStrap();
 		}
 
 		if (input.CompareNoCase(_T("dump")) == 0)
@@ -76,41 +71,90 @@ void go()
 	}
 }
 
-void SigInt_Handler(int action)
+void SignalHandler(int action)
 {
 	fprintf(stdout, "Signal %d caught", action);
 }
 
 int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
 {
-	int nRetCode = 0;
-	HMODULE hModule = ::GetModuleHandle(NULL);
+	if (startup() != 0)
+		return 1;
 
+	CString arg, cmd;
+	myOS = NULL;
+	init();
 
-	if (hModule != NULL)
+	for (int i = 1; i < argc; i++)
 	{
-		// initialize MFC and print and error on failure
-		if (!AfxWinInit(hModule, NULL, ::GetCommandLine(), 0))
+		arg = argv[i];
+		if ((arg[0] == '-') || (arg[0] == '/'))
 		{
-			// TODO: change error code to suit your needs
-			_tprintf(_T("Fatal Error: MFC initialization failed\n"));
-			nRetCode = 1;
+			arg = arg.Mid(1);
+			if (arg.CompareNoCase(_T("?")) == 0)
+			{
+				printf("usage: fcon -? -bs -include:fileName");
+			}
+			else if (arg.CompareNoCase(_T("bs")) == 0)
+			{
+				cmd.Format(_T(".bs"), arg);
+				myOS->ParseInput(cmd);
+			}
+			else if (arg.Left(8).CompareNoCase(_T("include:")) == 0)
+			{
+				arg = arg.Mid(8);
+				cmd.Format(_T("include %s"), arg);
+				myOS->ParseInput(cmd);
+			}
 		}
-		else
-		{
-			signal(SIGABRT, SigInt_Handler);
-			signal(SIGBREAK, SigInt_Handler);
-			signal(SIGTERM, SigInt_Handler);
-			signal(SIGINT, SigInt_Handler);
-			fp = stdin;
-			go();
-		}
+	}
+
+	return go();
+}
+
+void process_arg(CString& arg)
+{
+}
+
+int startup()
+{
+	HMODULE hModule = ::GetModuleHandle(NULL);
+	if (hModule == NULL)
+	{
+		_tprintf(_T("Fatal Error: GetModuleHandle failed\n"));
+		return 1;
+	}
+	// initialize MFC and print and error on failure
+	if (!AfxWinInit(hModule, NULL, ::GetCommandLine(), 0))
+	{
+		// TODO: change error code to suit your needs
+		_tprintf(_T("Fatal Error: MFC initialization failed\n"));
+		return 2;
+	}
+	return 0;
+}
+
+int init()
+{
+
+	if (myOS != NULL)
+	{
+		delete myOS;
+		myOS = NULL;
 	}
 	else
 	{
-		_tprintf(_T("Fatal Error: GetModuleHandle failed\n"));
-		nRetCode = 1;
+		signal(SIGABRT, SignalHandler);
+		signal(SIGBREAK, SignalHandler);
+		signal(SIGTERM, SignalHandler);
+		signal(SIGINT, SignalHandler);
 	}
 
-	return nRetCode;
+	fp = stdin;
+	myOS = new ForthOS(MEMORY_SIZE);
+	myOS->input_fp = stdin;
+	myOS->output_fp = stdout;
+	myOS->BootStrap();
+
+	return 0;
 }
